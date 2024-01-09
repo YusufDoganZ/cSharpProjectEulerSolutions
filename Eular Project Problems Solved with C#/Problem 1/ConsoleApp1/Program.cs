f527ef666be4c753f2086ecc08703c854e75b1dd
Yusuf Doğan <130250874+YusufDoganZ@users.noreply.github.com>
2024-01-09 21:59:48 +0300

    C# Eular Project Problem Solved
    Solving euler project problems with c#

---
+++ Eular Project Problems Solved with C#/Problem 1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿int x = 0;
+
+for (int i = 1;i<1000;i++)
+{
+    if (i % 3 == 0 || i % 5 == 0)
+    {
+        x += i;
+    }
+    else
+        continue;
+}
+Console.WriteLine("Problem Solved Answer = "+x);
+Console.ReadLine();
---
+++ Eular Project Problems Solved with C#/Problem 2/Problem 2/Program.cs
@@ -0,0 +1,13 @@
+﻿int x =0,y = 0,z = 0;
+for (int i = 1;i<4000000;)
+{
+    z = x;
+    x = i;
+    x -= z;
+    if (i % 2 == 0)
+        y += i;
+    i = x + i;
+
+}
+Console.WriteLine("Problem Solved Answer = " + y);
+Console.ReadLine();
